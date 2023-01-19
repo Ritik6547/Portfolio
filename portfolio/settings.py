52f54a562796f059ec757ab9be6e6997d7dc704f
Ritik <alphaop6547@gmail.com>
2023-01-19 22:26:31 +0530

    test

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u**@%cb(%*og+!!+h^!-jpzpq0g1(9ipx%m2)jyikp#1emqc9-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['app.alphasite.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'dynamic/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
